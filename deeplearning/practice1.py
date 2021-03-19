e55ef94af9cea367a4bd13279ad2a37fcaac8127
minshogi <80640280+minshogi@users.noreply.github.com>
2021-03-19 17:31:55 +0900

    git add .

---
+++ deeplearning/practice1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+
+
+def lossfunc(yhat, y):
+    j=0
+    dw1 = 0
+    dw2 = 0
+    db = 0
+
+def gen_samples(m):
+    x1 = np.array()pyt
+    x2 = np.array()
+    y = np.array()
